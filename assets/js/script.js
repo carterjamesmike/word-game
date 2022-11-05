59e5d6f385c279113120072c0c9798dc2554d4db
James Carter <carterjamesmike@gmail.com>
2022-11-04 22:35:41 -0500

    Minor split functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,47 @@
-var url = "https://hp-api.herokuapp.com/api/characters"
-
+var url = "https://hp-api.herokuapp.com/api/characters";
+var wordsEl = document.getElementById('words');
 
 queryData();
 nameArr = [];
+singleArr = [];
+
 
 function queryData () {
-
     fetch(url)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            for (var i = 0; i <data.length; i++)
-            nameArr.push(data[i].name);
+            for (var i = 0; i <data.length; i++) {
+                nameArr.push(data[i].name);
+            }
+        renderArray();
         })
 };
-console.log(nameArr);
+
+function renderArray () {
+    const randomWords = [];
+    for (let i = 0; i < 5; i++) {
+        randomWords.push(nameArr[Math.floor((Math.random() * nameArr.length))]);
+        let li = document.createElement('li')
+        li.innerHTML = randomWords[i];
+        wordsEl.appendChild(li);
+    }
+    split(randomWords);
+}
+
+function split (randomWords) {
+    for (let i = 0; i < randomWords.length; i++) {
+        singleArr.push(randomWords[i].split(""));
+        let li = document.createElement('li');
+        li.innerHTML = singleArr[i];
+        wordsEl.appendChild(li);
+    }
+}
+
+console.log(singleArr);
+
+
+
+
+
