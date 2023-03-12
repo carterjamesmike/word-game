984681ff4ec81d5ce95515c5e8b12ef7c7b1ada1
James Carter <carterjamesmike@gmail.com>
2023-03-12 15:11:11 -0500

    Functions for random words and shuffling letters complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import React from 'react'
+import Anagram from './components/Anagram.jsx'
 
 function App() {
 
   return (
     <div className="App">
       <h1>React App</h1>
+      <Anagram />
     </div>
   )
 }
--- src/components/Anagram.jsx
+++ src/components/Anagram.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { normalMode } from '../data/normal.js'
+
+const normal = normalMode;
 
 const Anagram = () => {
+const [wordOne, setWordOne] = useState('')
+const [wordTwo, setWordTwo] = useState('')
+const [shuffledWord, setShuffledWord] = useState('')
+
+//Function to randomoly select two words from the normal mode array
+const randomWords = () => {
+    const randomWordOne = normal[Math.floor(Math.random() * normal.length)]
+    const randomWordTwo = normal[Math.floor(Math.random() * normal.length)]
+    setWordOne(randomWordOne)
+    setWordTwo(randomWordTwo)
+    //console.log(randomWordOne.name)
+    shuffle(randomWordOne, randomWordTwo)
+}
+
+//Function that takes the two words and randomoly shuffles the letters into one word without spaces and all caps
+const shuffle = (randomWordOne, randomWordTwo) => {
+    const wordOneLetters = randomWordOne.name.split('')
+    const wordTwoLetters = randomWordTwo.name.split('')
+    const combinedLetters = wordOneLetters.concat(wordTwoLetters)
+    const shuffledLetters = combinedLetters.sort(() => Math.random() - 0.5)
+    const shuffledWord = shuffledLetters.join('').toUpperCase().replace(/\s/g, '')
+    setShuffledWord(shuffledWord)
+}
+
+
   return (
-    <div>Anagram</div>
+    <div>
+        <h1>Anagram</h1>
+        <button onClick={randomWords}>Start Game</button>
+        <div>
+            <h1>{wordOne.name}</h1>
+            <h1>{wordTwo.name}</h1>
+            <h1>{shuffledWord}</h1>
+        </div>
+    </div>
   )
 }
 
