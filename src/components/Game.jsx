747740b44068ce855d41fdb073a1e4896ddd1bc3
James Carter <carterjamesmike@gmail.com>
2023-03-17 10:10:07 -0500

    compare functionaly finished

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { normalMode } from '../data/normal.js'
+import { useForm } from 'react-hook-form'
 
 const normal = normalMode;
 
@@ -8,6 +9,10 @@ const [wordOne, setWordOne] = useState('')
 const [wordTwo, setWordTwo] = useState('')
 const [shuffledWord, setShuffledWord] = useState('')
 const shuffledWordArray = shuffledWord.split('')
+const [userInputOne, setUserInputOne] = useState('')
+const [userInputTwo, setUserInputTwo] = useState('')
+const [finalAnswer, setFinalAnswer] = useState('')
+const { register, handleSubmit } = useForm()
 
 //Function to randomoly select two words from the normal mode array
 const randomWords = () => {
@@ -21,13 +26,39 @@ const randomWords = () => {
 
 //Function that takes the two words and randomoly shuffles the letters into one word without spaces and all caps
 const shuffle = (randomWordOne, randomWordTwo) => {
+    const combinedWords = randomWordOne.name + randomWordTwo.name
+    const joinedAnswer = combinedWords.toUpperCase().replace(/\s/g, '')
+    setFinalAnswer(joinedAnswer)
     const wordOneLetters = randomWordOne.name.split('')
     const wordTwoLetters = randomWordTwo.name.split('')
-    const combinedLetters = wordOneLetters.concat(wordTwoLetters)
-    const shuffledLetters = combinedLetters.sort(() => Math.random() - 0.5)
+    const combinedLettersArr = wordOneLetters.concat(wordTwoLetters)
+    const shuffledLetters = combinedLettersArr.sort(() => Math.random() - 0.5)
     const shuffledWord = shuffledLetters.join('').toUpperCase().replace(/\s/g, '')
     setShuffledWord(shuffledWord)
 };
+
+//Function that will compare the user input to the two words
+const compareWords = () => {
+    console.log(`User Input One: ${userInputOne}`)
+    console.log(`User Input Two: ${userInputTwo}`)
+    console.log(`Word One: ${wordOne.name}`)
+    console.log(`Word Two: ${wordTwo.name}`)
+    const combinedOne = userInputOne + userInputTwo
+    const guessOne = combinedOne.toUpperCase().replace(/\s/g, '')
+    const cpmbinedTwo = userInputTwo + userInputOne
+    const guessTwo = cpmbinedTwo.toUpperCase().replace(/\s/g, '')
+
+    console.log(`Guess One: ${guessOne}`)
+    console.log(`Guess Two: ${guessTwo}`)
+    console.log(`Final Answer: ${finalAnswer}`)
+
+    if (guessOne === finalAnswer || guessTwo === finalAnswer) {
+        alert('You Win!')
+    } else {
+        alert('Try Again')
+    }
+}
+
 
 
 
@@ -40,7 +71,7 @@ const shuffle = (randomWordOne, randomWordTwo) => {
             <h1>{wordTwo.name}</h1>
             <h1>{shuffledWord}</h1>
         </div>
-        <div className='flex flex-row'>
+        <div className='flex flex-row flex-wrap justify-center '>
             {/* Map through the shuffledWordArray to create a container for each item */}
             {shuffledWordArray.map((letter, index) => {
                 return (
@@ -52,10 +83,23 @@ const shuffle = (randomWordOne, randomWordTwo) => {
 
         </div>
         {/* Two input fields that will compare user inputs to wordOne and wordTwo */}
-        <div>
-            <input type='text' />
-            <input type='text' />
-            </div>
+        <form onSubmit={handleSubmit(compareWords)}>
+        <div className='flex flex-col'>
+            <input
+            // {...register('userInputOne', { required: true })}
+            onChange={(e) => setUserInputOne(e.target.value)}
+            className='p-2 m-2'
+             type='text' />
+            <input
+            // {...register('userInputTwo', { required: true })}
+            onChange={(e) => setUserInputTwo(e.target.value)}
+            className='p-2 m-2'
+             type='text' />
+        </div>
+        <button type='submit'>Submit</button>                                           
+        </form>
+
+
     </div>
   )
 }
